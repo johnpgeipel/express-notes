0e1b20a619c1d682dab08537fbdcd3d9eea5f3c8
john geipel <47228329+johnpgeipel@users.noreply.github.com>
2022-11-17 13:44:33 -0600

    Add files via upload

---
+++ notes.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function refreshDb(notesArray) {
+    fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(notesArray, null, 2), err => {
+        if (err) {
+            throw err;
+        }
+    })
+}
+
+function makeNewNote(newNote, notesArray) {
+    notesArray.push(newNote);
+    fs.writeFileSync(path.join(__dirname, "db/db.json"), JSON.stringify(notesArray, null, 2), err => {
+        if (err) {
+            throw err;
+        }
+    });
+    return newNote;
+}
+
+module.exports = { refreshDb, makeNewNote }
---
+++ routes/apiRoute.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const db = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
+const { refreshDb, makeNewNote } = require('../notes')
+
+router.get('/notes', (req, res) => {
+    res.json(db);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = makeNewNote(req.body, db);
+    res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    let note
+
+    db.map((element, index) => {
+      if (element.id == id) {
+        note = element;
+        db.splice(index, 1);
+        refreshDb(db);
+        return res.json(note);
+      } 
+    })
+});
+
+module.exports = router;
